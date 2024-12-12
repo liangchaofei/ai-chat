93557fc93912436fa3ba0203c96f1e9f6c8d01e9
梁朝飞 <liangchaofei@guokechuzhi.com>
2024-12-12 09:58:47 +0800

    feat: mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ input, items }),
+      body: JSON.stringify({ input }),
     });
 
     const data = await res.json();
